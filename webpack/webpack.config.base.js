64f9a24f431fcf5294bf6bb7944e6115ce1ee4c2
yoongeemin <yoongeemin@gmail.com>
2016-02-23 17:22:43 -0500

    Update webpack.config.base.js

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -3,7 +3,6 @@ var path = require("path");
 var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var fs = require("fs");
-var hotMiddlewareScript = "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true";
 
 // Resolve binary dependency in node modules
 var nodeModules = {};
@@ -19,24 +18,30 @@ module.exports = {
 	target: "node",
 
 	entry: {
-		server: [ path.join(__dirname, "..", "server", "server.js"), hotMiddlewareScript ],
-		// app: [ path.join(__dirname, "..", "app", "index.js"), hotMiddlewareScript ]
+		server: [ path.join(__dirname, "..", "server", "server.js") ],
+		authentication: [ path.join(__dirname, "..", "client", "authentication.jsx") ],
+		app: [ path.join(__dirname, "..", "client", "app.jsx") ]
 	},
 
 	output: {
 		path: path.join(__dirname, "..", "static", "build"),
-		filename: "[name].dev.js"
+		filename: "[name].js"
 	},
 
 	externals: nodeModules,
+
+	resolve: {
+		extensions: ["", ".js", ".jsx", ".css", ".scss"]
+	},
 
 	plugins: [
 		// new webpack.optimize.CommonsChunkPlugin("common", "common.bundle.js"),
 		new ExtractTextPlugin("[name].css"),
 
 		new webpack.ProvidePlugin({
-			$: 					"jquery",
-			jQuery: 			"jquery",
+			$: 		"jquery",
+			jQuery: "jquery",
+			_: 		"lodash",
 		})
 		// 	React: 				"react",
 		// 	ReactDOM: 			"react-dom",

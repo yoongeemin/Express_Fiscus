7592a281e0a3d2722a25c1aba40b0ef1e1e81161
yoongeemin <yoongeemin@gmail.com>
2016-02-24 18:26:58 -0500

    Update client.jsx

--- app/client.jsx
+++ app/client.jsx
@@ -1,18 +1,31 @@
-import Authentication from "./containers/authentication";
-import { createStoreWithMiddleware } from "./utils/utils";
-import authenticationReducer from "./reducers/userReducer";
+import { configureStore } from "./utils/utils";
 import { Provider } from "react-redux";
+import { Router, Route, IndexRoute, browserhistory } from "react-router";
 
-const authenticationStore = createStoreWithMiddleware(
-	ThunkMiddleware,
-	LoggerMiddleware()
-)(authenticationReducer);
+import * as containers from "./containers/index";
+import reducers from "./reducers/index";
+import { SignIn, SignUp } from "./components/index";
+
+const store = configureStore(reducers, browserHistory);
+
+const authentication = (nextState, replace, callback) => {
+	if (!authenticated) {
+		replace("/signin");
+	}
+	callback();
+};
 
 ReactDOM.render(
 	(
-		<Provider store={authenticationStore}>
-			<Authentication />
+		<Provider store={store}>
+			<Router history={browserHistory}>
+				<Route path="/" component={App}>
+					<IndexRoute component={DashBoard} onEnter={authentication} />
+					<Route path="/signin" component={SignIn} />
+					<Route path="/signup" component={SignUp} />
+				</Route>
+			</Router>
 		</Provider>
 	),
-	document.getElementById("authentication")
+	document.getElementById("app")
 );

fe4067b2888c0cfebc1bc760fb58eeea892b1d8a
yoongeemin <yoongeemin@gmail.com>
2016-02-26 14:49:59 -0500

    Update webpack.config.dev.js

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -5,10 +5,10 @@ var _ = require("lodash");
 config.debug = true;
 config.devtool = "eval-source-map";
 
-// Add webpack dev server
+// Add webpack hot reloading
 _.each(config.entry, function(value, key) {
 	key = key.concat([ 
-		"webpack-dev-server/client?http://localhost:" + config.port,
+		"webpack-hot-middleware/client"		
 		"webpack/hot/only-dev-server"
 	]);
 });
@@ -16,9 +16,6 @@ _.each(config.entry, function(value, key) {
 config.plugins = config.plugins.concat([
 	new webpack.HotModuleReplacementPlugin(),
 	new webpack.NoErrorsPlugin(),
-	new webpack.DefinePlugin({
-		__ENV__: "DEV"
-	})
 ]);
 
 module.exports = config;

46cec78062465907b9995db25e77abe8a7289bb9
yoongeemin <yoongeemin@gmail.com>
2016-03-03 17:17:16 -0500

    Create watch.js

---
+++ gulp/watch.js
@@ -0,0 +1,12 @@
+var webpack = require("webpack");
+var gutil = require("gulp-util");
+
+module.exports = function(config) {
+	return function(callback) {
+		webpack(config).watch(1000, function(err, stats) {
+			if (err) throw new gutil.PluginError("webpack", err);
+			gutil.log("[webpack] ", stats.toString());
+			callback();
+		});
+	};
+};

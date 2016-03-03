54d68b221abfd3d853fa8acd718e5f7e22fd954b
yoongeemin <yoongeemin@gmail.com>
2016-03-02 21:33:33 -0500

    Update client.jsx

--- app/client.jsx
+++ app/client.jsx
@@ -1,34 +1,39 @@
 import { configureStore } from "./utils/utils";
 import { Provider } from "react-redux";
 import { Router, Route, IndexRoute, browserhistory } from "react-router";
+import { syncHistoryWithStore } from "react-router-redux";
+import { SignIn, Dashboard} from "./components/index";
+import { App, Authentication, SignUp} from "./containers/index";
 
 import * as containers from "./containers/index";
 import reducers from "./reducers/index";
 
-
 const store = configureStore(reducers, browserHistory);
+const history = syncHistoryWithStore(browserHistory, store)
 
 const authentication = (nextState, replace) => {
 	authenticated = false;
 	if (!authenticated) {
-		replace({
-			pathname: '/auth',
-			state: { nextPathname: nextState.location.pathname }
-		});
+		replace("/signin");
+	}
+	else {
+		replace("/dashboard");
 	}
 };
 
 ReactDOM.render(
 	(
 		<Provider store={store}>
-			<Router history={browserHistory}>
+			<Router history={history}>
 				<Route path="/" component={App}>
-					<IndexRoute component={DashBoard} onEnter={authentication} />
-					<Route path="/auth" component={Authentication} />
+					<IndexRoute onEnter={authentication} />
+					<Route path="/signin" component={Authentication} />
 					<Route path="/signup" component={SignUp} />
+					<Route path="/dashboard" component={Dashboard}>
 				</Route>
 			</Router>
 		</Provider>
 	),
 	document.body
 );
+

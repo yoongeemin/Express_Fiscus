7e77a8604753473b06e0f96d80e572c74b0150ca
yoongeemin <yoongeemin@gmail.com>
2016-03-03 17:18:30 -0500

    Create dev.js

---
+++ webpack/dev.js
@@ -0,0 +1,16 @@
+var path = require("path");
+var base = require("./base");
+
+base.debug = true;
+base.devtool = "eval-cheap-module-source-map";
+
+base.module.preLoaders = [
+	{
+		test: /\.jsx?$/,
+		exclude: /node_modules/,
+		loader: "eslint-loader"
+	}
+];
+
+
+module.exports = base;

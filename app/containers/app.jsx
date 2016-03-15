ba9a1b704de33ac36f056baf612abdf9885c1486
yoongeemin <yoongeemin@gmail.com>
2016-03-15 17:47:27 -0400

    Update app.jsx

--- app/containers/app.jsx
+++ app/containers/app.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
-import NavBar from "../components/navbar"
+import NavBar from "./navbar";
+import { getQuotes } from "../actions/index";
 
 class App extends React.Component {
-	render() {
-		return (
-			<div id="app">
-				<NavBar authenticated={false} />
+    componentDidMount() {
+        this.props.dispatch(getQuotes());
+    }
 
-				{this.props.children}
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div id="app">
+                <NavBar profile={this.props.profile} quotes={this.props.quotes} />
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
-function mapStateToProps(state) {
-	return {
-		// quotes: state.quoteReducer.quotes,
-		// profile: state.fiscusReducer.profile,
-		// apps: state.fiscusReducer.apps,
-		// activeApp: state.fiscusReducer.activeApp
+App.propTypes = {
+    dispatch: React.PropTypes.func.isRequired,
+    profile: React.PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => {
+    return {
+        profile: state.userReducer.profile,
+        quotes: state.quoteReducer.quotes,
 	};
-}
+};
 
 export default connect(mapStateToProps)(App);

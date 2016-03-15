b8b1e372e2522f49a1ecfc735286c6037517acd4
yoongeemin <yoongeemin@gmail.com>
2016-03-15 17:49:34 -0400

    Update index.js

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { routeReducer as routing } from "react-redux";
 import userReducer from "./userReducer";
+import quoteReducer from "./quoteReducer";
 
 export default combineReducers({
 	userReducer,
+	quoteReducer,
 	routing,
 });

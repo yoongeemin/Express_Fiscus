4f2b2abdc906df1ace5b6d4a33523c29edc91a52
yoongeemin <yoongeemin@gmail.com>
2016-03-15 17:46:30 -0400

    Update index.js

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,3 @@
-export { Ticker } from "./tickers";
+export { Authentication } from "./authentication";
 export { Dashboard } from "./dashboard";
-export { SignIn } from "./signIn";
+export { Ticker } from "./tickers";
